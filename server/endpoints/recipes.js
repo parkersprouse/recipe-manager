0321ab94540ef78a9377de2e738635a8c8fe95e4
Parker Sprouse <poker158149@gmail.com>
2017-09-20 13:51:47 -0500

    Slightly cleaned up endpoint - could definitely use more

--- server/endpoints/recipes.js
+++ server/endpoints/recipes.js
@@ -17,49 +17,37 @@ function addOrUpdateRecipe(func, req, res, next) {
   let ingredientsEmpty = !ingredients || _.isEmpty(ingredients);
   let stepsEmpty = !steps || _.isEmpty(steps);
 
-  if (!stepsEmpty)
-    for (let i = 0; i < steps.length; i++)
-      if (!steps[i] || validator.isEmpty(steps[i]))
-        stepsEmpty = true;
+  let stepsState = [];
+  let ingredientsState = [];
 
-  if (!ingredientsEmpty) {
-    for (var i in ingredients) {
-      if (ingredients.hasOwnProperty(i)) {
-        const ing = ingredients[i];
-        if ((!ing || _.isEmpty(ing)) ||
-            (!ing.name || validator.isEmpty(ing.name)) ||
-            (!ing.measurement || validator.isEmpty(ing.measurement)) ||
-            (!ing.amount || validator.isEmpty(ing.amount)))
-          ingredientsEmpty = true;
+  for (let i = 0; i < steps.length; i++) {
+    if (!steps[i] || validator.isEmpty(steps[i])) {
+      stepsState.push('invalid');
+      stepsEmpty = true;
+    }
+    else {
+      stepsState.push('valid');
+    }
+  }
+
+  for (var i in ingredients) {
+    if (ingredients.hasOwnProperty(i)) {
+      const ing = ingredients[i];
+      if (!ing.name || validator.isEmpty(ing.name) || !ing.measurement || validator.isEmpty(ing.measurement) || !ing.amount || validator.isEmpty(ing.amount)) {
+        ingredientsState.push({
+          name: !ing.name || validator.isEmpty(ing.name) ? 'invalid' : 'valid',
+          measurement: !ing.measurement || validator.isEmpty(ing.measurement) ? 'invalid' : 'valid',
+          amount: !ing.amount || validator.isEmpty(ing.amount) ? 'invalid' : 'valid'
+        });
+        ingredientsEmpty = true;
+      }
+      else {
+        ingredientsState.push({name: 'valid', measurement: 'valid', amount: 'valid'});
       }
     }
   }
 
   if (titleEmpty || ingredientsEmpty || stepsEmpty) {
-    const stepsState = [];
-    for (let i = 0; i < steps.length; i++)
-      if (!steps[i] || validator.isEmpty(steps[i]))
-        stepsState.push('invalid');
-      else
-        stepsState.push('valid');
-
-    const ingredientsState = [];
-    for (var i in ingredients) {
-      if (ingredients.hasOwnProperty(i)) {
-        const ing = ingredients[i];
-        if ((!ing.name || validator.isEmpty(ing.name)) ||
-            (!ing.measurement || validator.isEmpty(ing.measurement)) ||
-            (!ing.amount || validator.isEmpty(ing.amount)))
-          ingredientsState.push({
-            name: !ing.name || validator.isEmpty(ing.name) ? 'invalid' : 'valid',
-            measurement: !ing.measurement || validator.isEmpty(ing.measurement) ? 'invalid' : 'valid',
-            amount: !ing.amount || validator.isEmpty(ing.amount) ? 'invalid' : 'valid'
-          });
-        else
-          ingredientsState.push({name: 'valid', measurement: 'valid', amount: 'valid'});
-      }
-    }
-
     res.status(constants.http_bad_request)
       .json({
         status: 'failure',
