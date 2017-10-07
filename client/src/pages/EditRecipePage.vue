8d08bb8272ae717d2d0530a2defa7656b9a8854f
Parker Sprouse <poker158149@gmail.com>
2017-10-07 15:50:47 -0500

    Back to recipe button

--- client/src/pages/EditRecipePage.vue
+++ client/src/pages/EditRecipePage.vue
@@ -7,6 +7,9 @@
           <div v-if="!recipe"></div>
           <div v-else-if="recipe === -1">That recipe doesn't exist</div>
           <div v-else>
+            <div style="margin-top: -1rem; margin-bottom: 1rem;">
+              <a :href="'/recipes/' + $route.params.id"><i class="fa fa-angle-double-left" aria-hidden="true"></i> Back to recipe</a>
+            </div>
             <h3 class="title is-3">Edit Recipe</h3>
             <recipe-form :recipe="recipe" />
           </div>
