2ac7520d4cef325820bdd18fc18d0845cf68ab11
Parker Sprouse <poker158149@gmail.com>
2017-10-10 22:11:08 -0500

    Changed blue color and removed ribbon from view recipe

--- client/src/pages/ViewRecipePage.vue
+++ client/src/pages/ViewRecipePage.vue
@@ -13,16 +13,17 @@
               <div>
                 <a :href="'/recipes/' + recipe.id + '/edit'" class="button is-info">Edit Recipe</a>
               </div>
-              <div v-if="!!recipe.date" class="top-right">
-                <span class="tag is-white">
+              <div class="top-right">
+                <span class="tag is-dark" v-if="!!recipe.date">
                   {{ makeDateReadable(recipe.date) }}
+                </span>
+                <span class="tag is-dark">
+                  {{ recipe.private ? 'Private' : 'Public' }}
                 </span>
               </div>
             </div>
 
-            <section class="hero is-primary is-bold has-ribbon view-recipe-body" style="margin-bottom: 1.5rem;">
-              <div class="ribbon is-info tooltip is-tooltip-left" data-tooltip="This recipe can only be viewed by you" v-if="recipe.private">Private</div>
-              <div class="ribbon is-info tooltip is-tooltip-left" data-tooltip="This recipe can be viewed by anyone" v-else>Public</div>
+            <section class="hero is-primary is-bold view-recipe-body" style="margin-bottom: 1.5rem;">
               <div class="hero-body">
                 <div class="container">
                   <h1 class="title">
