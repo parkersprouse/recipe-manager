3c279e910ee57c2a555a35c86ede006e54404943
Parker Sprouse <poker158149@gmail.com>
2017-12-02 10:30:22 -0600

    Added calculator input fields

--- client/src/components/MeasurementCalculator.vue
+++ client/src/components/MeasurementCalculator.vue
@@ -1,11 +1,40 @@
 <template>
   <div>
-    Test
+    <div class="field has-addons">
+      <div class="control is-expanded has-icons-left">
+        <input type="text" v-model="valueToConvert" placeholder="Value" class="input" />
+        <span class="icon is-small is-left">
+          <i class="fa fa-calculator"></i>
+        </span>
+      </div>
+      <div class="control">
+        <div class="select">
+          <select v-model="measurement">
+            <option>Ounces</option>
+            <option>Tablespoons</option>
+          </select>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'measurement-calculator'
+    name: 'measurement-calculator',
+    data: function() {
+      return {
+        valueToConvert: null,
+        measurement: 'Tablespoons'
+      }
+    },
+    watch: {
+      valueToConvert: function(newValue) {
+        console.log(newValue);
+      },
+      measurement: function(newMeasurement) {
+        console.log(newMeasurement);
+      }
+    }
   }
 </script>
--- client/src/pages/ConvertorPage.vue
+++ client/src/pages/ConvertorPage.vue
@@ -22,7 +22,7 @@
             <tr><td>1 Milliliter</td><td></td></tr>
             <tr><td>1 Ounce</td><td></td></tr>
             <tr><td>1 Pint</td><td></td></tr>
-            <tr><td>1 Pound</td><td></td></tr>
+            <tr><td>1 Pound</td><td>16 Ounces</td></tr>
             <tr><td>1 Quart</td><td></td></tr>
             <tr><td>1 Tablespoon</td><td></td></tr>
             <tr><td>1 Teaspoon</td><td></td></tr>
@@ -38,7 +38,7 @@
 
 <script>
   export default {
-    name: 'calculator-page'
+    name: 'converter-page'
   }
 </script>
 
